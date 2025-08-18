4d2dceb65ed4ad774c58591b8ead9a12c59a1bc9
KSPOG <kratosmaximusosrs@gmail.com>
2025-08-19 01:00:34 +0200

    Use sandboxbot package for client classes

--- src/main/java/main/java/sandboxbot/MainSwing.java
+++ src/main/java/main/java/sandboxbot/MainSwing.java
@@ -1,15 +1,20 @@
-package main.java.main.java.sandboxbot;
+package main.java.sandboxbot;
 
 import main.java.sandboxbot.plugins.RandomWalker;
+import sandboxbot.ClientFrame;
 import javax.swing.*;
 
 public class MainSwing {
     public static void main(String[] args) {
-        main.java.sandboxbot.World world = new main.java.sandboxbot.World(18, 12);
+        World world = new World(18, 12);
 
-        main.java.sandboxbot.BotCore bot = new main.java.sandboxbot.BotCore(15, world);
+        BotCore bot = new BotCore(15, world);
         bot.register(new RandomWalker(), true);
+        new Thread(bot::start).start();
 
-        SwingUtilities.invokeLater(() -> new ClientFrame(bot, world, 40, 30));
+        SwingUtilities.invokeLater(() -> {
+            ClientFrame frame = new ClientFrame();
+            frame.setVisible(true);
+        });
     }
 }

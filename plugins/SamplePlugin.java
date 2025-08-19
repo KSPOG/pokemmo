eaee05eec5d7d2d863a6b533d0af86548fe07c50
KSPOG <kratosmaximusosrs@gmail.com>
2025-08-20 00:49:39 +0200

    docs: clarify sample plugin

---
+++ plugins/SamplePlugin.java
@@ -0,0 +1,11 @@
+/**
+ * Simple demonstration plugin that logs when run.
+ */
+public class SamplePlugin implements ClientPlugin {
+    @Override
+    public void run(Client client) {
+        // Log through the client's logger to verify plugin execution
+        client.getLogger().info("SamplePlugin executed");
+    }
+}
+
